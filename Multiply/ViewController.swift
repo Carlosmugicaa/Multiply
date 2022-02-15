1cb2d9c2b293fcb8852c92fe25db01387e88f83b
Carlos Mugica <cmugica4@cloud.com>
2022-02-15 14:16:28 -0700

    map

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var number1: UITextField!
+    
+    
+    @IBOutlet weak var number2: UITextField!
+    
+    
+    @IBOutlet weak var outcome1: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func actionmath(_ sender: Any) {
+        var product1 = number1.text ?? ""
+    
+        var product2 = number2.text ?? ""
+        
+        var num1 = Int(product1) ?? 0
+    
+        var num2 = Int(product2) ?? 0
+    
+        var multiply = num1 * num2
+        
+        outcome1.text = "\(multiply)"
+                          
+    }
+        
+    
+    
+    
+    
 }
+
 
